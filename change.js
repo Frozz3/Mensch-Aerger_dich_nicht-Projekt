a10164bece1f8bfa61328caf2e5b977931fdcb0e
Frozz3 <105207692+Frozz3@users.noreply.github.com>
2023-01-13 15:22:18 +0100

    Button Spieler Finish

--- change.js
+++ change.js
@@ -1,9 +1,17 @@
-function change(className) {
-    var buttons = document.getElementsByClassName(className);
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function(){
-            this.innerHTML = "Neuer Text";
-        });
-    }
-}
-changeButtonText("my-button-class");
+function changeText1() { 
+    document.getElementById("button_ready1").innerHTML = "Ready"; 
+} 
+function changeText2() { 
+    document.getElementById("button_ready2").innerHTML = "Ready"; 
+} 
+function changeText3() { 
+    document.getElementById("button_ready3").innerHTML = "Ready"; 
+} 
+function changeText4() { 
+    document.getElementById("button_ready4").innerHTML = "Ready"; 
+} 
+
+
+
+
+
