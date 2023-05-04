b14636617dfc11b781fcf3ecf23b1c95193bd260
Hauke <hauke.otterstaetter@gmail.com>
2023-05-04 18:11:14 +0200

    format code

--- client/collabsible.js
+++ client/collabsible.js
@@ -1,18 +1,15 @@
 
 function collabsible() {
-let toggleCollapsible = document.getElementById("toggleColl");
-toggleCollapsible.addEventListener("click", () => {
-
-    let div1 = document.getElementsByClassName("content-player")[0];
-    let div2 = document.getElementsByClassName("content-buttons")[0];
-
-    if (div1.classList.contains("visible")) {
-
-        div1.classList.remove("visible");
-        div2.classList.remove("visible");
-    } else {
-
-        div1.classList.add("visible");
-        div2.classList.add("visible");
-    }
-});}
+    let toggleCollapsible = document.getElementById("toggleColl");
+    toggleCollapsible.addEventListener("click", () => {
+        let div1 = document.getElementsByClassName("content-player")[0];
+        let div2 = document.getElementsByClassName("content-buttons")[0];
+        if (div1.classList.contains("visible")) {
+            div1.classList.remove("visible");
+            div2.classList.remove("visible");
+        } else {
+            div1.classList.add("visible");
+            div2.classList.add("visible");
+        }
+    });
+}
